ea73e729adb6ddbf69f1d752fd91af2b8abf7f19
Mike Limberger <oh-hai@miker.media>
2022-12-25 13:46:40 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# E-mail Validator
+
+Validate those e-mails with regex! (Not perfect but a start). This is a modified version of my [reddit](https://www.reddit.com/r/regex/comments/otd9py/reasonable_email_validation_with_regex/) / [regex101](https://regex101.com/r/0fMpfy/1) post I made awhile back.
+
+## Featuring
+
+- Free space mode for clean organized regex action
+- Regex grammers for resuable patterns
+- Positive and negative lookarounds to do the dirty work
+
+## To Do
+
+- Refractor for even more use cases!
+- Add more methods to injest e-mails (csv, json, newline delimited)
---
+++ email-validator.pl
@@ -0,0 +1,135 @@
+#!/usr/bin/env perl
+
+my $email = Validate::Email->load();
+
+$email->go();
+
+package Validate::Email
+{
+	
+	use feature qw|say|;
+	use Term::ANSIColor qw(:constants);
+	
+	sub load()
+	{
+		
+		my $class = shift;
+		my $regex = qr`(?xim)
+		
+		# regex grammar
+		(?(DEFINE)
+
+			# no punctuation
+			(?<punct>[[:punct:]])
+	
+			# valid e-mail characters
+			(?<alnum>[[:alnum:]\.+-]+)
+		
+		)
+		
+		^
+		# e-mail domain ends with letters
+		(?=.*\.[A-Z]+$)
+		
+		# e-mail contains numbers, letters, + , -
+		(?=(?&alnum)@(?&alnum))
+		
+		# No double @ signs
+		(?!.*@.*@)
+		
+		# dosn't end with numbers
+		(?!.*?@.*\.\d+$)
+		
+		# domain has more then 2 characters
+		(?!.*\.{2,})
+
+		# no starting punctuation
+		(?!(?&punct))
+		
+		# no punctuation near @ sign (left)
+		(?!.*(?&punct)@)
+		
+		# no punctuation near @ sign (right)
+		(?!.*@(?&punct))
+		
+		# rules above modify wildcard
+		.*
+		
+		`;
+		
+		return bless ({ regex => $regex }, $class);
+		
+	}
+	
+	sub go()
+	{
+		
+		local $Term::ANSIColor::AUTORESET = 1;
+		my $self = shift;
+
+		while (1)
+		{
+
+			print q|enter e-mail to validate: |;
+			
+			chomp (my $email = <STDIN>);
+			
+			if ($email =~ m~q(uit)?~i)
+			{
+				say BLUE "> quitting!";
+				exit 0;
+			}
+			
+			if ($email =~ m`$$self{regex}`)
+			{
+				say GREEN qq|> ${email} is valid!|;
+			}
+			else
+			{
+				say RED qq|> ${email} is invalid!|;
+			}
+			
+		}
+	
+	}
+
+}
+
+
+__END__
+URL: https://regex101.com/r/0fMpfy/1
+
+# SAMPLE DATA: VALID E-MAILS
+
+email@example.web
+email@example.com
+firstname.lastname@example-ya.com
+email@subdomain.example.com
+firstname+lastname@example.com
+email@123.123.123.com
+1234567890@example.com
+email@example-one.com
+email@example.name
+email@example.museum
+email@example.co.jp
+firstname-lastname@example.com
+amazing@website.amazing
+
+# SAMPLE DATA: INVALID E-MAILS
+
+plainaddress
+#@%^%#$@#$@#.com
+@example.com
+Joe Smith <email@example.com>
+email.example.com
+email@example@example.com
+.email@example.com
+email.@example.com
+email..email@example.com
+あいうえお@example.com
+email@example.com (Joe Smith)
+email@example
+email@-example.com
+email@111.222.333.44444
+email@example..com
+Abc..123@example.com
